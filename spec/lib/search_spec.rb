ad67824abc2350ad46997e244011d41fb7026f9f
Nikita Plyusnin <nplyusnin78@gmail.com>
2022-09-28 22:17:13 +0300

    add specs for simple search

--- spec/lib/search_spec.rb
+++ spec/lib/search_spec.rb
@@ -3,20 +3,22 @@ require "spec_helper"
 require_relative "../../lib/search.rb"
 
 describe Search do
-  describe "#find_binary" do
-    context "value is exists" do
-      it { expect(subject.find_binary([1, 2, 3], 1)).to eq(true) }
-      it { expect(subject.find_binary([1, 2, 3], 2)).to eq(true) }
-      it { expect(subject.find_binary([1, 2, 3], 3)).to eq(true) }
-      it { expect(subject.find_binary([1, 2, 3, 4, 5, 6, 7, 8], 2)).to eq(true) }
-      it { expect(subject.find_binary([1, 2, 3, 4, 5, 6, 7, 8], 6)).to eq(true) }
-    end
-
-    context "value don't exists" do
-      it { expect(subject.find_binary([1, 3, 5], 2)).to eq(false) }
-      it { expect(subject.find_binary([1, 3, 5], 4)).to eq(false) }
-      it { expect(subject.find_binary([1, 3, 9, 21, 99, 125, 127, 138], 2)).to eq(false) }
-      it { expect(subject.find_binary([1, 3, 9, 21, 99, 125, 127, 138], 130)).to eq(false) }
+  %w(find find_binary).each do |method_name|
+    describe "##{method_name}" do
+      context "value is exists" do
+        it { expect(subject.send(method_name, [1, 2, 3], 1)).to eq(true) }
+        it { expect(subject.send(method_name, [1, 2, 3], 2)).to eq(true) }
+        it { expect(subject.send(method_name, [1, 2, 3], 3)).to eq(true) }
+        it { expect(subject.send(method_name, [1, 2, 3, 4, 5, 6, 7, 8], 2)).to eq(true) }
+        it { expect(subject.send(method_name, [1, 2, 3, 4, 5, 6, 7, 8], 6)).to eq(true) }
+      end
+  
+      context "value don't exists" do
+        it { expect(subject.send(method_name, [1, 3, 5], 2)).to eq(false) }
+        it { expect(subject.send(method_name, [1, 3, 5], 4)).to eq(false) }
+        it { expect(subject.send(method_name, [1, 3, 9, 21, 99, 125, 127, 138], 2)).to eq(false) }
+        it { expect(subject.send(method_name, [1, 3, 9, 21, 99, 125, 127, 138], 130)).to eq(false) }
+      end
     end
   end
 end
